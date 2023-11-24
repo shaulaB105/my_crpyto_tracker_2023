e2d49b9938f76edd638c6ca9fc7998099defe82b
taskok2 <87404682+shaulaB105@users.noreply.github.com>
2023-11-24 22:19:52 +0900

    23.1124

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -5,7 +5,7 @@ import Coin from "../components/Coin/Coin";
 
 function Router (){
     return (
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Switch>
                 <Route path="/:coinId">
                     <Coin />
